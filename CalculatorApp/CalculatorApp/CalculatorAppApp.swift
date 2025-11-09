6a0d857e7d8194de51ae3d2e7c3c17bb10aed42c
Daniel Bros <danielbros41@gmail.com>
2025-11-09 17:48:16 +0100

    init commit

---
+++ CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by user279425 on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
