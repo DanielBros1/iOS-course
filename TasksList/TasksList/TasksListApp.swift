aa019cc521a9cd63a64a8eaa4c985e6a3ad66fc7
Daniel Bros <danielbros41@gmail.com>
2025-11-26 22:49:58 +0100

    tasksList initialize

---
+++ TasksList/TasksList/TasksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksListApp.swift
+//  TasksList
+//
+//  Created by user279425 on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TasksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
