853a684566f5cb9f3f068c7d2dc2b684499b248c
Daniel Bros <danielbros41@gmail.com>
2026-01-09 16:01:54 +0100

    stripe task init

---
+++ stripe/stripe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stripe
+//
+//  Created by user279425 on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stripe/stripe/stripeApp.swift
@@ -0,0 +1,17 @@
+//
+//  stripeApp.swift
+//  stripe
+//
+//  Created by user279425 on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct stripeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
