480d674a0689c1da28e68c1b67936e9777a636c8
Daniel Bros <danielbros41@gmail.com>
2026-01-17 23:14:04 +0100

    7: Test project init and mock

---
+++ ShoppingList/ShoppingListTests/ShoppingListTests.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingListTests.swift
+//  ShoppingListTests
+//
+//  Created by user279425 on 1/17/26.
+//
+
+import Testing
+
+struct ShoppingListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
